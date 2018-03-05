88194c225f6b5157c08c0c811e5238a0e2c61b4a
Harald Hoyer <harald@redhat.com>
2018-03-05 17:16:15 +0100

    fixed TestMore()

--- orgexamplemore/methods.go
+++ orgexamplemore/methods.go
@@ -17,34 +17,53 @@ func NewInterface() Interface {
 }
 
 func (intf *Interface) TestMore(call varlink.Call) error {
-	var in TestMore_In
-	err := call.GetParameters(&in)
-	if err != nil {
-		return call.ReplyError("org.varlink.service.InvalidParameter", varlink.InvalidParameter_Error{Parameter: "parameters"})
+	if !call.WantsMore() {
+		return call.ReplyError("org.varlink.service.InvalidParameter",
+			varlink.InvalidParameter_Error{Parameter: "more"})
 	}
 
-	call.Reply(&varlink.ServiceOut{
-		Continues:  true,
-		Parameters: TestMore_Out{State: State{Start: true}},
-	})
+	var in TestMore_In
+	if err := call.GetParameters(&in); err != nil {
+		return call.ReplyError("org.varlink.service.InvalidParameter",
+			varlink.InvalidParameter_Error{Parameter: "parameters"})
+	}
+
+	if err := call.Reply(&varlink.ServiceOut{Continues: true,
+		Parameters: struct {
+			State interface{} `json:"state"`
+		}{State: struct {
+			Start bool `json:"start"`
+		}{Start: true}}}); err != nil {
+		return err
+	}
 
 	for i := int64(0); i < in.N; i++ {
-		call.Reply(&varlink.ServiceOut{
+		if err := call.Reply(&varlink.ServiceOut{
 			Continues:  true,
 			Parameters: TestMore_Out{State: State{Progress: int64(i * 100 / in.N)}},
-		})
+		}); err != nil {
+			return err
+		}
 		time.Sleep(time.Second)
 	}
-	call.Reply(&varlink.ServiceOut{
-		Continues:  true,
-		Parameters: TestMore_Out{State: State{Progress: int64(100)}},
-	})
+
+	if err := call.Reply(&varlink.ServiceOut{
+		Continues: true,
+		Parameters: struct {
+			State interface{} `json:"state"`
+		}{State: struct {
+			Progress int64 `json:"progress"`
+		}{Progress: int64(100)}}}); err != nil {
+		return err
+	}
 
 	return call.Reply(&varlink.ServiceOut{
-		Continues:  false,
-		Parameters: TestMore_Out{State: State{End: true}},
-	})
-
+		Continues: false,
+		Parameters: struct {
+			State interface{} `json:"state"`
+		}{State: struct {
+			Start bool `json:"end"`
+		}{Start: true}}})
 }
 
 func (intf *Interface) StopServing(call varlink.Call) error {
